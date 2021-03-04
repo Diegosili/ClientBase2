0dde16a23ad3926b41e5c17f162ae35be1b35758
siliprandi.diego <siliprandi.diego@TAVE-PC30.OSDBSESTO.local>
2021-03-04 14:07:57 +0100

    add feature

--- src/clientbase2/ClientBase2.java
+++ src/clientbase2/ClientBase2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package clientbase;
+package clientbase2;
 
 import java.io.*;
 import java.net.*;
@@ -13,9 +13,13 @@ import java.util.logging.Logger;
 
 /**
  *
+ * Sviluppare un applicativo ClientServer che generi tanti Thread quanti dichiarati dall'utente attraverso un Thread a lui dedicato.
+ * Ogni Thread sommerà a una variabile globale contenuta nel server il proprio valore incrementale assegnatogli dal Server da 1 a n, dove n è il numero dei Thread;
+ * Il Server killerà ogni processo dopo aver svolto l'operazione, quando tutti i processi saranno killati il Server stamperà a console il risultato
+ * 
  * @author siliprandi.diego
  */
-public class ClientBase {
+public class ClientBase2 {
 
     /**
      * @param args the command line arguments
@@ -27,28 +31,18 @@ public class ClientBase {
             Socket server = new Socket("127.0.0.1", 5500);
 
             PrintWriter scrittore = new PrintWriter(server.getOutputStream(), true);
-            BufferedReader riceve = new BufferedReader(new InputStreamReader(server.getInputStream()));
             Scanner tastiera = new Scanner(System.in);
-            String s = "";
+            int s = 0;
             
-            while (!s.equals("exit")) {
-                System.out.println("Inserire frase da inviare al server:");
-                s = tastiera.nextLine();
-                System.out.println("frase inserita: " + s);
-                scrittore.println(s);
-                String risposta = riceve.readLine();
-                System.out.println("risposta del server: " + risposta);
-                
-                risposta = risposta.split(": ")[1];
-                System.out.println("Hai inserito "+risposta+" caratteri");
-            }
-
-            riceve.close();
+            System.out.println("Inserire numero di Thread da attivare:");
+            s = tastiera.nextInt();
+            scrittore.println(s);
+            
             server.close();
             System.out.println("chiusura connessione");
             
         } catch (IOException ex) {
-            Logger.getLogger(ClientBase.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ClientBase2.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
